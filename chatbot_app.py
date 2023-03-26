2ab9bf2a9c5782c0e47bbae0297065a378540571
trevor2718 <tdupwork1@gmail.com>
2023-03-26 16:37:11 +0200

    Minor correction

--- chatbot_app.py
+++ chatbot_app.py
@@ -22,7 +22,7 @@ def get_text():
     input_text = st.text_input("You: ",value="", key="input",placeholder="Please ask your questions about the weather in the region: ")
     return input_text 
 
-region  = st.text_input("You: ",value="", key="input_",placeholder=first_message)
+region  = st.text_input("Please enter the region: ",value="", key="input_",placeholder=first_message)
 if(region and not st.session_state['generated']):
     # Get the full address and the coordinates based on information provided by user 
     full_address, latitude, longitude  = get_coordinates(region)
