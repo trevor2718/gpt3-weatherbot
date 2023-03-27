be18f0c275b5c6ceeed44cd7f3a4c0b58ab09340
trevor2718 <tdupwork1@gmail.com>
2023-03-27 14:17:33 -0400

    added a chabot, but the template exceed max tokens, need to fix it

---
+++ chatbot/chat_message_history.py
@@ -0,0 +1,42 @@
+import sys,os
+sys.path.append(os.path.realpath('..'))
+
+from langchain.llms import OpenAI
+from api_key import openapi_key
+OpenAI.api_key = openapi_key
+
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI, VectorDBQA
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI, LLMChain, PromptTemplate
+
+class ChatBot:
+    def __init__(self,template):
+        self.template = template
+        self.history = ""
+
+        self.prompt = PromptTemplate(
+            input_variables=["chat_history", "human_input"], 
+            template=template
+        )
+        self.memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+    
+        llm_chain = LLMChain(
+                        llm=OpenAI(temperature=0.0,), 
+                        prompt=self.prompt, 
+                        verbose=True, 
+                        memory=self.memory,
+                    )
+        self.agent = llm_chain
+        return 
+    
+    def get_reply(self,message):
+        """
+        for the given input message from user get the reply of the chatbot 
+        """
+        reply = self.agent.predict(human_input=message)
+
+        return reply
--- chatbot_app.py
+++ chatbot_app.py
@@ -5,6 +5,8 @@ from streamlit_chat import message
 from get_location import  get_coordinates
 from get_weather import get_weather
 from process_json.extract_data import get_template
+from chatbot.chat_message_history import ChatBot
+
 info = True 
 
 
@@ -26,14 +28,25 @@ def get_text():
     return input_text 
 
 region  = st.text_input("Please enter the region: ",value="", key="input_",placeholder=first_message)
-if(region and not st.session_state['generated']):
+if(region):
     # Get the full address and the coordinates based on information provided by user 
     full_address, latitude, longitude  = get_coordinates(region)
     # Get weather from the api call 
     weather_data = get_weather(latitude,longitude)
     template_weather_data = get_template(weather_data)
-    print(template_weather_data)
+
+    template = """  You are a chatbot having a conversation about the above weather data. 
+                    You are a polite chatbot and very interactive. You must provide very accurate and correct information, 
+                    If you are unsure anout an answer just ask could you provide some more information, the information you have given is insufficient. 
+
+    {chat_history} Human: {human_input} Chatbot:"""
+    template = template + template_weather_data
+
+    chatbot = ChatBot(template=template)
+    print(chatbot)
+
     current_temp = weather_data["currently"]["temperature"]
+if(region and not st.session_state['generated']):
     if(full_address):
         st.session_state.generated.append(f"You have asked for details in region: {region} \n " \
                                             f"Full address: {full_address} \n " \
@@ -49,7 +62,7 @@ if(region and not st.session_state['generated']):
 
 user_input = get_text()
 if user_input:
-    output = "Testing mode on"
+    output = chatbot.get_reply(user_input)
     st.session_state.generated.append(output)
     st.session_state.past.append(user_input)
 
